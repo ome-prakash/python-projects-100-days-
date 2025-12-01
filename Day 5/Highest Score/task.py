dd1f68e618e5dee1221070625d89324f8f7cbc01
Ome prakash <35755839+ome-prakash@users.noreply.github.com>
2025-12-01 13:37:11 +0530

    Add files via upload

---
+++ Day 5/Highest Score/task.py
@@ -0,0 +1,12 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+tot=sum(student_scores)
+
+
+maxx= max(student_scores)
+
+m
+for score in student_scores:
+    if score > max:
+        max=score
+print(max)
+
